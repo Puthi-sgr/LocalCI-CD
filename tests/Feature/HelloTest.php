e7884670191b22f76ad4da8d4582b2ccfc02ef5b
Puthi-sgr <chethaputhi@gmail.com>
2025-08-09 15:19:51 +0700

    Create a simple end point

---
+++ tests/Feature/HelloTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class HelloTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function test_example(): void
+    {
+        $response = $this->get('/');
+
+        $response->assertOk()
+                    ->assertHeader('Content-Type', 'text/plain; charset=UTF-8')
+                    ->assertJson([ 
+                        'message' => 'hello'
+                    ]);
+    }
+}
